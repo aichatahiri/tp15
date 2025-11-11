11e0a684321dd25d99e233eb09d107db66f0a952
aichatahiri <aichatahiri502@gmail.com>
2025-11-11 21:43:22 +0100

    Premier commit TP15

---
+++ banque-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO compte (id, solde, date_creation, type) VALUES (1, 1500.0, '2024-11-18', 'COURANT');
+INSERT INTO compte (id, solde, date_creation, type) VALUES (2, 3000.0, '2024-11-17', 'EPARGNE');
+INSERT INTO compte (id, solde, date_creation, type) VALUES (3, 2000.0, '2024-11-19', 'COURANT');
